cb9a7730109f2da76967409a61042fa3614ecb6d
Max Luan <max_luan@foxmail.com>
2023-04-06 20:03:47 +0800

    add tailwind, hello world.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,17 +7,24 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 class="text-2xl mb-4">{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
+  <div class="card p-4 bg-white rounded-lg shadow-md">
+    <div class="flex items-center justify-center">
+      <button
+        class="px-4 py-2 rounded-md bg-gray-200 hover:bg-gray-300"
+        @click="count++"
+      >
+        count is: {{ count }}
+      </button>
+    </div>
+    <p class="mt-4">
       Edit
-      <code>components/HelloWorld.vue</code> to test HMR
+      <code class="text-gray-500">components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
 
-  <p>
+  <p class="mt-6">
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
       >create-vue</a
@@ -25,14 +32,16 @@ const count = ref(0)
   </p>
   <p>
     Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank"
+      >Volar</a
+    >
     in your IDE for a better DX
   </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p class="mt-6 text-gray-500">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
+.card {
+  border: 1px solid #e2e8f0;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import vue from '@vitejs/plugin-vue'
 import electron from 'vite-plugin-electron'
 import renderer from 'vite-plugin-electron-renderer'
 import pkg from './package.json'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
@@ -14,6 +16,14 @@ export default defineConfig(({ command }) => {
   const sourcemap = isServe || !!process.env.VSCODE_DEBUG
 
   return {
+    css: {
+      postcss: {
+        plugins: [
+          tailwindcss,
+          autoprefixer,
+        ],
+      },
+    },
     plugins: [
       vue(),
       electron([
