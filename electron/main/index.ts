935b4491c01598bfa9b07d0f7bf9becb3876f6c1
Max Luan <maxluan.misc@gmail.com>
2023-04-08 19:32:27 +0800

    tabbed browserview.

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow, shell, ipcMain } from 'electron'
+import { app, BrowserWindow, shell, ipcMain, BrowserView } from 'electron'
 import { release } from 'node:os'
 import { join } from 'node:path'
 
@@ -40,7 +40,48 @@ const preload = join(__dirname, '../preload/index.js')
 const url = process.env.VITE_DEV_SERVER_URL
 const indexHtml = join(process.env.DIST, 'index.html')
 
-async function createWindow() {
+const views = {
+  default: null,
+  internet: null,
+  tabbar: null,
+}
+
+async function createWindow() {  
+  const defaultView = new BrowserView({
+    webPreferences: {
+      preload,
+      // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
+      // Consider using contextBridge.exposeInMainWorld
+      // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  })
+  const internetView = new BrowserView(
+    {
+      webPreferences: {
+        preload,
+        // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
+        // Consider using contextBridge.exposeInMainWorld
+        // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
+        nodeIntegration: true,
+        contextIsolation: false,
+      },
+    },
+  )
+
+  // Tab bar doesn't need a preload. Otherwise, it takes a long time to load
+  const tabbarView = new BrowserView({
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  })
+
+  views.default = defaultView
+  views.internet = internetView
+  views.tabbar = tabbarView
+
   win = new BrowserWindow({
     title: 'Main window',
     icon: join(process.env.PUBLIC, 'favicon.ico'),
@@ -55,27 +96,49 @@ async function createWindow() {
   })
 
   if (process.env.VITE_DEV_SERVER_URL) { // electron-vite-vue#298
-    win.loadURL(url)
+    tabbarView.webContents.loadFile(join(process.env.PUBLIC, 'tabbar.html'))
+    defaultView.webContents.loadURL(url)
     // Open devTool if the app is not packaged
-    win.webContents.openDevTools()
+    // defaultView.webContents.openDevTools()
   } else {
-    win.loadFile(indexHtml)
+    tabbarView.webContents.loadFile(join(process.env.DIST, 'tabbar.html'))
+    defaultView.webContents.loadFile(indexHtml)
   }
 
   // Test actively push message to the Electron-Renderer
-  win.webContents.on('did-finish-load', () => {
-    win?.webContents.send('main-process-message', new Date().toLocaleString())
+  defaultView.webContents.on('did-finish-load', () => {
+    defaultView?.webContents.send('main-process-message', new Date().toLocaleString())
   })
 
   // Make all links open with the browser, not with the application
-  win.webContents.setWindowOpenHandler(({ url }) => {
+  defaultView.webContents.setWindowOpenHandler(({ url }) => {
     if (url.startsWith('https:')) shell.openExternal(url)
     return { action: 'deny' }
   })
   // win.webContents.on('will-navigate', (event, url) => { }) #344
+
+  function handleResize() {
+    const { width, height } = win.getBounds()
+    const viewsHeight = height - 50
+    
+    tabbarView.setBounds({x: 0, y: viewsHeight - 50, width, height: 50})
+    defaultView.setBounds({x: 0, y: 0, width, height: viewsHeight - 50})
+    internetView.setBounds({x: 0, y: 0, width, height: viewsHeight - 50})
+  }
+  
+  handleResize()
+
+  win.on('resize', () => {
+    handleResize()
+  })
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow).then(() => {
+  // manually trigger a resize, otherwise, the views will not be rendered. Don't know why
+  win.setSize(win.getSize()[0], win.getSize()[1] + 1)
+  win?.addBrowserView(views.default)
+  win?.addBrowserView(views.tabbar)
+})
 
 app.on('window-all-closed', () => {
   win = null
@@ -99,19 +162,12 @@ app.on('activate', () => {
   }
 })
 
-// New window example arg: new windows url
-ipcMain.handle('open-win', (_, arg) => {
-  const childWindow = new BrowserWindow({
-    webPreferences: {
-      preload,
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
-  })
-
-  if (process.env.VITE_DEV_SERVER_URL) {
-    childWindow.loadURL(`${url}#${arg}`)
-  } else {
-    childWindow.loadFile(indexHtml, { hash: arg })
+ipcMain.on('change-view', (event, viewName) => {
+  const view = views[viewName]
+  if (view) {
+    // don't remove tabbar
+    win?.removeBrowserView(views.default)
+    win?.removeBrowserView(views.internet)
+    win?.addBrowserView(view)
   }
 })
